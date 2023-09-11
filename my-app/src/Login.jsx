c0e9a14017a48da62fb6b46cccecf8ef8f620b79
j1nxxxx <122502658+j1nxxxx@users.noreply.github.com>
2023-09-11 22:35:20 +1000

    GUI Update
    Updated the interface of the login page, will work on functionality tomorrow, me no feel good irl :(, barely any sleep

--- my-app/src/Login.jsx
+++ my-app/src/Login.jsx
@@ -3,12 +3,18 @@ export const Login = () => {
     const[username, setUser] = useState('');
     const[password, setPass] = useState('');
     return (
+        <div className="login-form">
        <form>
-        <label for="text">Username</label>
-        <input value={username} type="text"></input>
-        <label for="password">Password</label>
-        <input value={pass} type="password"></input>
-        <button>Login</button>
+        <div class="input-container">
+        <label for="text"><b>Username&nbsp;&nbsp;&nbsp;</b></label>
+        <input value={username} class="input" type="text"></input>
+        </div>
+        <div class="input-container">
+        <label for="password"><b>Password&nbsp;&nbsp;&nbsp;</b></label>
+        <input value={password} class="input" type="password"></input>
+        </div>
+        <button type="submit" class="button">Login</button>
        </form>
+       </div>
     )
 }
