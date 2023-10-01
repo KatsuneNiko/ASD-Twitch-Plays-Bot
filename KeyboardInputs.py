5f9f91f598b910c30d00fa74ca9d8ccfb4c4b2b2
nekonatic <david.phung22@hotmail.com>
2023-10-01 17:09:04 +1100

    add keyboard

--- KeyboardInputs.py
+++ KeyboardInputs.py
@@ -4,13 +4,31 @@ import time
 import keyboard
 import TwitchConnect
 import pydirectinput
+import os
 
 
 
 #takes the user message as a parameter.
 def KeyboardInputs(message):
     testMessage = message.lower()
+    with open('KeywordsToKeyboard.txt') as f:
+        for l_no, lines in enumerate(f):
+            if testMessage in lines:
+                singleLine = lines
+                singleLine = singleLine.split(",")
+                inputType = str(singleLine[0]).lower()
+                duration = str(singleLine[1]).lower()
+                keyword = str(singleLine[2]).lower()
+                keybind = str(singleLine[3]).lower()[:-1]
+                print(keybind)
+                if(inputType) == "press":
+                    pressKeyNew(keybind)
+                elif(inputType.lower()) == "hold":
+                    holdKey(keybind, duration)
 
+    
+
+    '''
     #hardcode keywords to keyboard inputs
     if testMessage == 'hello':
         typeMessage(testMessage)
@@ -32,6 +50,36 @@ def KeyboardInputs(message):
         pressKeyNew('p')
     elif testMessage == 'time':
         print(time.time())
+    '''     
+    
+def addKeybind():
+    holdOrPress = ""
+    ##Validation: user must only be able to select hold or press.
+    while holdOrPress.lower() != "hold" and holdOrPress.lower() != "press":
+        os.system('cls')
+        holdOrPress = input("Hold or Press?: ")
+        if holdOrPress.lower() != "hold" and holdOrPress.lower() != "press":
+            os.system('cls')
+            print("Invalid answer, \"" + holdOrPress + "\" is not a valid answer, please try again. ")
+            input("Press any key to continue... ")
+        else:
+            break
+
+    if holdOrPress.lower() == 'hold':
+        ##Validation: user must only be able to select ints or valid floats
+        duration = input("Duration in seconds? e.g. 1 or 1.5: ")
+    ##Validation: can not be an existing duplicate
+    chosenKeyword = input("Input exact keyword: ")
+    ##Validation: valid keyboard bind
+    allocatedKeybind = input("What key is this binded to?: ")
+
+    file1 = open("KeywordsToKeyboard.txt", "a")
+    if holdOrPress.lower() == 'hold':
+        newLine = holdOrPress + "," + duration + "," + chosenKeyword + "," + allocatedKeybind
+    else:
+        newLine = holdOrPress + "," + "nil" + "," + chosenKeyword + "," + allocatedKeybind 
+    file1.write(str(newLine)+'\n')
+    file1.close()
 
 def pressKey(key):
     keyboard.press(key)
