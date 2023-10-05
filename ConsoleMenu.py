02db69348d386c615d0f0a66031a9583f7f7a43b
KatsuneNiko <crystallizedlumina@gmail.com>
2023-10-05 23:32:54 +1100

    Minor changes to code

--- ConsoleMenu.py
+++ ConsoleMenu.py
@@ -3,60 +3,60 @@ import threading
 import TwitchConnect
 import KeyboardInputs
 
+if __name__ == '__main__':
+    userInput = ''
+    t1 = threading.Thread(target=TwitchConnect.twitch)
+    ##t2 = TwitchConnect.twitch()
 
-userInput = ''
-t1 = threading.Thread(target=TwitchConnect.twitch)
-##t2 = TwitchConnect.twitch()
+    while userInput != 'exit':
+        print("Main menu")
+        print("Press the following to trigger function")
+        print("1 - Start chatbot")
+        print("2 - Stop chatbot")
+        print("3 - View Keybinds")
+        print("4 - Add keybind")
+        print("5 - Delete keybind")
+        print("exit - exit")
+        userInput = input("Input one of the following: ")
+        os.system('cls')
+        match userInput:
+            case '1':
+                if t1.is_alive() and TwitchConnect.pauseEvent.is_set() == False:
+                    print("Chatbot is already active")
+                elif t1.is_alive() and TwitchConnect.pauseEvent.is_set() == True:
+                    TwitchConnect.pauseEvent.clear()
+                    print("Chatbot has now been restarted")
+                else:
+                    t1.daemon = True
+                    t1.start()
+                    os.system('cls')
+                    print("Chatbot is now active")
+                input("Press any key to continue...")
+            
+            case '2':
+                if t1.is_alive():
+                    TwitchConnect.pauseEvent.set()
+                    print("Chatbot has been stopped")
+                else:
+                    print("Chatbot was not active")
+                input("Press any key to continue...")
 
-while userInput != 'exit':
-    print("Main menu")
-    print("Press the following to trigger function")
-    print("1 - Start chatbot")
-    print("2 - Stop chatbot")
-    print("3 - View Keybinds")
-    print("4 - Add keybind")
-    print("5 - Delete keybind")
-    print("exit - exit")
-    userInput = input("Input one of the following: ")
-    os.system('cls')
-    match userInput:
-        case '1':
-            if t1.is_alive() and TwitchConnect.pauseEvent.is_set() == False:
-                print("Chatbot is already active")
-            elif t1.is_alive() and TwitchConnect.pauseEvent.is_set() == True:
-                TwitchConnect.pauseEvent.clear()
-                print("Chatbot has now been restarted")
-            else:
-               t1.daemon = True
-               t1.start()
-               os.system('cls')
-               print("Chatbot is now active")
-            input("Press any key to continue...")
-        
-        case '2':
-            if t1.is_alive():
-                TwitchConnect.pauseEvent.set()
-                print("Chatbot has been stopped")
-            else:
-                print("Chatbot was not active")
-            input("Press any key to continue...")
+            case '3':
+                f = open("KeywordsToKeyboard.txt", "r")
+                print(f.read())
+                input("Press any key to continue...")
 
-        case '3':
-            f = open("KeywordsToKeyboard.txt", "r")
-            print(f.read())
-            input("Press any key to continue...")
+            case '4':
+                KeyboardInputs.addKeybind()
+                print("Success!")
+                input("Press any key to continue...")
+            case '5':
+                KeyboardInputs.deleteKeybind()
+                print("Success!")
+                input("Press any key to continue...")
 
-        case '4':
-            KeyboardInputs.addKeybind()
-            print("Success!")
-            input("Press any key to continue...")
-        case '5':
-            KeyboardInputs.deleteKeybind()
-            print("Success!")
-            input("Press any key to continue...")
-
-        case 'exit':
-            TwitchConnect.exitEvent.set()
-            pass
-        
-    os.system('cls')
+            case 'exit':
+                TwitchConnect.exitEvent.set()
+                pass
+            
+        os.system('cls')
