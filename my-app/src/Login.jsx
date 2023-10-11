2a13e1b27699dbaf3428892770b677d4fb010b22
j1nxxxx <122502658+j1nxxxx@users.noreply.github.com>
2023-10-11 22:46:56 +1100

    backend validation for login

--- my-app/src/Login.jsx
+++ my-app/src/Login.jsx
@@ -1,52 +1,53 @@
 import React, { useState } from "react";
+import axios from "axios";
 
 export const Login = (props) => {
-  const [username, setUser] = useState("");
-  const [password, setPass] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
   const [error, setError] = useState("");
+  const [success, setSuccess] = useState("");
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    try {
-      const response = await fetch("supra-secret-shhhh.txt");
-      if (!response.ok) {
-        throw new Error("Failed to fetch user data."); //idk why this doesnt work lol
-      }
+  const handleLogin = (e) => { // Add 'e' as a parameter
+    e.preventDefault(); // Prevent the default form submission
+    console.log('Username:', username);
+    console.log('Password:', password);
 
-      const userData = await response.text();
-      const userLines = userData.split("\n");
-
-      for (const line of userLines) {
-        const [fileUsername, filePassword] = line.split(",");
-        if (fileUsername === username && filePassword === password) {
-          setError("LOGIN SUCCESSFUL!");
-          return;
-        }
-        else {
-            setError("Invalid username or password.");
-        }
-      }
-      
-    } catch {
-        //idk what to write here lol, works without anything as intended
+    if (username && password) {
+      // Use Axios here to make a POST request to your server
+      axios.post(`http://localhost:3521/login`, { username, password })
+      .then(response => {
+        console.log("Response from server:", response);
+        setSuccess("LET'S GO!");
+        setError("");
+      })
+        .catch(error => {
+          // Handle errors
+          setError("Login failed. Please check your credentials.");
+          setSuccess(""); 
+        });
+    } else {
+      setError('Both fields are required!');
+      setSuccess("");
     }
+    //<button type="button" class="button" onClick={() => props.onFormSwitch()}>Sign Up</button>
   };
+  
 
     return (
-        <div className="login-form">
-       <form onSubmit={handleSubmit}>
+      <div className="login-form">
+       <form onSubmit={handleLogin}>
         <div class="input-container">
         <label for="text"><b>Username&nbsp;&nbsp;&nbsp;</b></label>
-        <input class="input" type="text" value={username} onChange={(e) => setUser(e.target.value)}></input>
+        <input class="input" type="text" name="username" onChange={e => setUsername(e.target.value)} ></input>
         </div>
         <div class="input-container">
         <label for="password"><b>Password&nbsp;&nbsp;&nbsp;</b></label>
-        <input class="input" type="password" value={password} onChange={(e) => setPass(e.target.value)}></input>
+        <input class="input" type="password" name="password" onChange={e => setPassword(e.target.value)}></input>
         </div>
-        <button type="button" class="button" onClick={() => props.onFormSwitch()}>Register</button>
         <button type="submit" class="button">Login</button>
        </form>
        {error && <p className="error">{error}</p>}
+       {success && <p className="success">{success}</p>}
        </div>
     )
 };
--- my-app/src/Register.jsx
+++ my-app/src/Register.jsx
@@ -1,16 +1,55 @@
 import React, { useState } from "react";
+import axios from "axios";
 
 export const Register = (props) => {
-  const [username, setUser] = useState("");
-  const [password, setPass] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
   const [error, setError] = useState("");
+  const [success, setSuccess] = useState("");
 
+  const handleRegister = (e) => {
+    e.preventDefault(); // Prevent the default form submission
+  
+    if (username && password) {
+      // Create a data object to send to the server
+      const data = {
+        username: username,
+        password: password,
+      };
+  
+      // Make a POST request to your server for registration
+      axios.post("http://localhost:3521/login", data)
+        .then(response => {
+          // Handle a successful registration
+          console.log("Registration successful:", response.data);
+          setSuccess("Registration successful!");
+          // You can also redirect to the login page or perform other actions here.
+        })
+        .catch(error => {
+          // Handle registration errors
+          console.error("Registration failed:", error);
+          setError("Registration failed. Please check your input.");
+        });
+    } else {
+      setError('Both fields are required!');
+    }
+  };
 
-return (
-
-<form>
-    <button type="register" class="button" onClick={() => props.onFormSwitch('login')}>Login</button>
-    </form>
-)
-
+  return (
+    <div className="login-form">
+      <form onSubmit={handleRegister}>
+        <div class="input-container">
+          <label for="text"><b>Create Username&nbsp;&nbsp;&nbsp;</b></label>
+          <input class="input" type="text" name="username" value={username} onChange={(e) => setUsername(e.target.value)}></input>
+        </div>
+        <div class="input-container">
+          <label for="password"><b>Create Password&nbsp;&nbsp;&nbsp;</b></label>
+          <input class="input" type="password" name="password" value={password} onChange={(e) => setPassword(e.target.value)}></input>
+        </div>
+        <button type="button" class="button" onClick={() => props.onFormSwitch()}>Back to login</button>
+        <button type="submit" class="button">Sign Up!</button>
+      </form>
+      {error && <p className="error">{error}</p>}
+    </div>
+  );
 };
