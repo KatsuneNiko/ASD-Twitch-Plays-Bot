3648e53c6b1c3436ff2a3ea592e94e4ca0b6538b
nekonatic <david.phung22@hotmail.com>
2023-10-12 18:38:29 +1100

    Frontend can communicate with backend, check out the Style of play page. ensure that flask run and npm start are ran on two seperate instances of terminal

--- app.py
+++ app.py
@@ -1,6 +1,23 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask 
+##from backend import TwitchConnect
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+app = Flask(__name__) 
+
+# Pass the required route to the decorator. 
+@app.route("/hello") 
+def hello(): 
+    return "Hello, Welcome to GeeksForGeeks"
+
+@app.route("/StyleOfPlay") 
+def StyleOfPlay(): 
+	return {
+		"getSOP": "anarchy"
+    }
+	
+@app.route("/") 
+def index(): 
+	return "Homepage of GeeksForGeeks"
+
+if __name__ == "__main__": 
+	app.run(debug=True) 
+	app.run(port=5000)
--- src/pages/StyleOfPlay.js
+++ src/pages/StyleOfPlay.js
@@ -1,12 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react"
+
+
 
 const StyleOfPlay = () =>  {
+    const [SOP, setSOP] = useState([])
+
+    const fetchAPI = () => {
+        fetch('/StyleOfPlay').then(response => {
+            return response.json()
+          }).then(data => {
+            setSOP(data)
+            console.log(data);
+          })
+      }
+
+    useEffect(() => {
+        fetchAPI()
+      }, [])
+    
+
     return (
         <div>
             <h1>Style of Play</h1>
+            <text>Style of play is currently set to... {SOP.getSOP} </text>
         </div>
     );
+    
 };
+
+
 
 export default StyleOfPlay;
 
