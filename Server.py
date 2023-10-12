5e7c3720030b717d26571fa9403cbe627f8da762
KupoCoder <Frank.Yu@student.uts.edu.au>
2023-10-12 15:35:58 +1100

    Server.py update

--- Server.py
+++ Server.py
@@ -1,7 +1,9 @@
 from flask import Flask, jsonify
+from flask_cors import CORS  # <-- Add this import
 from MouseInputs import *
 
 app = Flask(__name__)
+CORS(app)  # <-- Add this line to enable CORS for the entire app
 
 @app.route('/create-text-file', methods=['POST'])
 def create_text_file_endpoint():
