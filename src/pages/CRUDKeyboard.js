733b13b37df87e33380dcf6b6d74ca21d70abb57
nekonatic <david.phung22@hotmail.com>
2023-10-12 20:45:52 +1100

    style of play controller

--- src/pages/CRUDKeyboard.js
+++ src/pages/CRUDKeyboard.js
@@ -1,11 +1,37 @@
 import React from "react";
+import { useState } from 'react';
+
 
 const CRUDKeyboard = () =>  {
     return (
         <div>
             <h1>CRUD Keyboard</h1>
+            <text>This page is used to add and delete keywords. {MyForm} </text> <br /> <br />
+            <button>Press to either switch between anarchy or democracy</button>
         </div>
     );
 };
+
+function MyForm() {
+    const [name, setName] = useState("");
+  
+    const handleSubmit = (event) => {
+      event.preventDefault();
+      alert(`The name you entered was: ${name}`)
+    }
+  
+    return (
+      <form onSubmit={handleSubmit}>
+        <label>Enter your name:
+          <input 
+            type="text" 
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </label>
+        <input type="submit" />
+      </form>
+    )
+  }
 
 export default CRUDKeyboard;
--- src/pages/StyleOfPlay.js
+++ src/pages/StyleOfPlay.js
@@ -1,52 +1,27 @@
 import React, { useEffect, useState } from "react"
 
+const StyleOfPlay = () =>  {    
+    
+    
 
-
-const StyleOfPlay = () =>  {
     const [SOP, setSOP] = useState([])
-
-    const fetchAPI = () => {
+    
         fetch('/StyleOfPlay').then(response => {
             return response.json()
           }).then(data => {
             setSOP(data)
             console.log(data);
           })
-      }
-
-    useEffect(() => {
-        fetchAPI()
-      }, [])
-      handleSubmit(event) {
-        const axios = require('axios');
-        const baseUrl = 'http://localhost:5000'
-    
-        axios.post('http://localhost:5000/api/create', JSON.stringify(params))
-            .end((error, response) => {
-                if (!error && response) {
-                    console.log('got a valid response from the server')
-                } else {
-                    console.log(`Error fetching data from the server: `, error)
-                }
-            });
-    
-        event.preventDefault();
-    }
       
+
     return (
         <div>
             <h1>Style of Play</h1>
             <text>Style of play is currently set to... {SOP.getSOP} </text> <br /> <br />
-            <text>Select one of the follow to switch modes... </text> <br />
-            <button onClick={() => switchToAnarchy()} >Anarchy mode</button>
-            <button onClick={() => switchToDemocracy()}>Democracy mode</button>
-
+            <button >Press to either switch between anarchy or democracy</button>
         </div>
     );
+}
     
-};
-
-
-
 export default StyleOfPlay;
 
