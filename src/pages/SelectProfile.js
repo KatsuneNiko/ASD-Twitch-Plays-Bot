8ed09c7adbfeff1c29a894da2716ce924593cf99
KatsuneNiko <104719855+KatsuneNiko@users.noreply.github.com>
2023-10-13 02:43:55 +1100

    wip

--- src/pages/SelectProfile.js
+++ src/pages/SelectProfile.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import styled from "styled-components";
 
 const SelectProfile = () =>  {
     const [profileNames, setProfileNames] = useState([]);
@@ -29,19 +30,74 @@ const SelectProfile = () =>  {
         setCurrentProfile(requestJson.currentProfile);
     }
 
+    const createProfile = async (profileName) => {
+        const request = await fetch('/ProfileManager', {
+            method: 'POST',
+            body: JSON.stringify({ 
+                method: 'create',
+                name: profileName
+            }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+        const requestJson = await request.json();
+        setProfileNames(requestJson.profileNames);
+        setCurrentProfile(requestJson.currentProfile);
+    }
+
+    const deleteProfile = async (profileName) => {
+        const request = await fetch('/ProfileManager', {
+            method: 'POST',
+            body: JSON.stringify({ 
+                method: 'delete',
+                name: profileName
+            }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+        const requestJson = await request.json();
+        setProfileNames(requestJson.profileNames);
+        setCurrentProfile(requestJson.currentProfile);
+    }
+
     return (
         <div>
-            <h1>Select Profile</h1>
-            Current profile is {currentProfile || 'unselected'}
-            {
-                profileNames.map((profileName, index) => (
-                    <div>
-                        <button key={index} onClick={() => setProfile(profileName)}>
-                            {profileName}
-                        </button>
-                    </div>
-                ))
-            }
+            <h1>Profile Manager</h1>
+            <div>
+                <h2>Select Profile</h2>
+                Current profile is {currentProfile || 'unselected'}
+                <table>
+                {
+                    profileNames.map((profileName, index) => (
+                        <tr key={index}>
+                            <td>
+                                {profileName}
+                            </td>
+                            <td>
+                                <button onClick={() => deleteProfile(profileName)}>
+                                    Select
+                                </button>
+                                <button onClick={() => deleteProfile(profileName)}>
+                                    Delete
+                                </button>
+                            </td>
+                        </tr>
+                    ))
+                }
+                </table>
+            </div>
+            <div>
+                <h2>Create Profile</h2>
+                <form onSubmit={(event) => {
+                    event.preventDefault();
+                    createProfile(event.target.profileName.value);
+                }}>
+                    <input name="profileName" type="text" />
+                    <button type="submit">Submit</button>
+                </form>
+            </div>
         </div>
     );
 };
