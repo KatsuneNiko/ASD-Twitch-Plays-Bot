ec057148621298479621b5ae7ab009f8b0b92fcb
KupoCoder <Frank.Yu@student.uts.edu.au>
2023-10-12 15:08:30 +1100

    Test changes

--- test_MouseInputs.py
+++ test_MouseInputs.py
@@ -1,13 +1,21 @@
-import sys
 import pytest
 from unittest.mock import patch, mock_open, MagicMock
-sys.modules['pyautogui'] = MagicMock()
-import MouseInputs
+
+# Mocking the pyautogui module
+pytestmark = pytest.mark.usefixtures("mock_pyautogui")
+
+@pytest.fixture
+def mock_pyautogui(monkeypatch):
+    mock = MagicMock()
+    monkeypatch.setattr("MouseInputs.pyautogui", mock)
+    return mock
 
 @pytest.fixture
 def mock_file():
     with patch("builtins.open", new_callable=mock_open) as mock_file:
         yield mock_file
+
+import MouseInputs
 
 def test_create_text_file(mock_file):
     MouseInputs.create_text_file()
@@ -23,34 +31,28 @@ def test_process_twitch_message_valid_command():
         MouseInputs.process_twitch_message("move_up")
         mock_mouse_inputs.assert_called_once_with("move_up")
 
+def test_moveMouse(mock_pyautogui):
+    mock_pyautogui.position.return_value = (0, 0)
+    MouseInputs.moveMouse(10, 20)
+    mock_pyautogui.moveTo.assert_called_once_with(10, 20, duration=0.2)
 
-def test_moveMouse():
-    with patch("MouseInputs.pyautogui") as mock_pyautogui:
-        mock_pyautogui.position.return_value = (0, 0)
-        MouseInputs.moveMouse(10, 20)
-        mock_pyautogui.moveTo.assert_called_once_with(10, 20, duration=0.2)
+def test_clickMouse(mock_pyautogui):
+    MouseInputs.clickMouse('left')
+    mock_pyautogui.click.assert_called_once_with(button='left')
 
-def test_clickMouse():
-    with patch("MouseInputs.pyautogui") as mock_pyautogui:
-        MouseInputs.clickMouse('left')
-        mock_pyautogui.click.assert_called_once_with(button='left')
+def test_scrollMouse(mock_pyautogui):
+    MouseInputs.scrollMouse(1)
+    mock_pyautogui.scroll.assert_called_once_with(1)
 
-def test_scrollMouse():
-    with patch("MouseInputs.pyautogui") as mock_pyautogui:
-        MouseInputs.scrollMouse(1)
-        mock_pyautogui.scroll.assert_called_once_with(1)
+def test_draw_circle(mock_pyautogui):
+    mock_pyautogui.position.return_value = (0, 0)
+    MouseInputs.draw_circle(100)
+    assert mock_pyautogui.moveTo.call_count > 0
 
-def test_draw_circle():
-    with patch("MouseInputs.pyautogui") as mock_pyautogui:
-        mock_pyautogui.position.return_value = (0, 0)
-        MouseInputs.draw_circle(100)
-        assert mock_pyautogui.moveTo.call_count > 0
-
-def test_click_coordinates():
-    with patch("MouseInputs.pyautogui") as mock_pyautogui:
-        MouseInputs.click_coordinates(100, 200)
-        mock_pyautogui.moveTo.assert_called_once_with(100, 200, duration=0.2)
-        mock_pyautogui.click.assert_called_once()
+def test_click_coordinates(mock_pyautogui):
+    MouseInputs.click_coordinates(100, 200)
+    mock_pyautogui.moveTo.assert_called_once_with(100, 200, duration=0.2)
+    mock_pyautogui.click.assert_called_once()
 
 def test_add_allowed_command(mock_file):
     MouseInputs.add_allowed_command("new_command")
