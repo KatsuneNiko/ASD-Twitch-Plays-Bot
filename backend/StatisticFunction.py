91bafe09c4394d5b7ce142affdbf1a326951e973
zipianli818 <100552310+zipianli818@users.noreply.github.com>
2023-11-02 16:33:31 +1100

    Update StatisticFunction.py

--- backend/StatisticFunction.py
+++ backend/StatisticFunction.py
@@ -1,12 +1,11 @@
 import socket
 import threading
 import time
-import TwitchConnect as TwitchConnect
 
 global user_count
 global message_count
 
-user_count = {}
+user_count = 0
 message_count = 0
 
 def update_stats(user, message):
@@ -18,9 +17,9 @@ def update_stats(user, message):
 
     # Update user statistics
     if "JOIN #" in user:
-                    user_count += 1  # Increment viewer_count when a user joins
+        user_count += 1  # Increment viewer_count when a user joins
     elif "PART #" in user:
-                    user_count -= 1  # Decrement viewer_count when a user leaves
+        user_count -= 1  # Decrement viewer_count when a user leaves
 
     # Update message count
     message_count += 1
@@ -32,18 +31,11 @@ def display_stats():
     while True:
         # Display the number of messages sent by each user and total message count
         print("----- User Message Statistics -----")
-        for user, count in user_count.items():
-            print(f"{user}: {count} messages")
-
-        print(f"Total User watching: {user_count}")
-        print("\n")
-
+        print(f"Total Users Watching: {user_count}")
         print(f"Total Messages: {message_count}")
         print("\n")
 
-        # Sleep for 30 seconds before displaying statistics again
-        # Display statistics every 10 seconds (adjust as needed)
-        time.sleep(30)
+        time.sleep(10)
 
 # Start a thread to display statistics
 t2 = threading.Thread(target=display_stats)
